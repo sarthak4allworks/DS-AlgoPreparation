01a55ed6f669ac2d2c0bcdc6101642cee9851b1c
SARTHAK AGARWAL <agarwalsarthak21@gmail.com>
2020-07-21 17:38:19 +0530

    Problem 8

--- MicroSoftPreparation/src/ProjectEulerProblems/Problem8.java
+++ MicroSoftPreparation/src/ProjectEulerProblems/Problem8.java
@@ -1,11 +1,31 @@
 package ProjectEulerProblems;
 
+import java.util.Scanner;
+
+//https://projecteuler.net/problem=8
 public class Problem8 
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		System.out.println("Start...");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		String str = s.next();
+		long mul = 1;
+		for(int i=0 ; i<str.length()-n ; i++)
+		{
+			long multi = 1;
+			for(int j=i ; j<i+n ; j++)
+			{
+				long num = str.charAt(j)-'0';
+				multi *= num;
+			}
+			if(multi>mul)
+			{
+				mul = multi;
+			}
+		}
+		System.out.println(mul);
+		s.close();
 	}
-
 }
