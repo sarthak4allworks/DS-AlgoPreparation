af4eeeb8df2d5f0551a118c5f3a668b7b1606226
SARTHAK AGARWAL <agarwalsarthak21@gmail.com>
2020-07-21 17:09:41 +0530

    Problem 10

--- MicroSoftPreparation/src/ProjectEulerProblems/Problem10.java
+++ MicroSoftPreparation/src/ProjectEulerProblems/Problem10.java
@@ -9,6 +9,33 @@ public class Problem10
 	{
 		System.out.println("Start...");
 		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		boolean[] arr = new boolean[n+1];
+		for(int i=0 ;i<=n ; i++)
+		{
+			arr[i] = true;
+		}
+		arr[0] = arr[1] = false;
+		for(int i=2 ; i<=n ; i++)
+		{
+			if(arr[i])
+			{
+				arr[i] = true;
+				for(int j=i*2 ; j<=n ; j+=i)
+				{
+					arr[j] = false;
+				}
+			}
+		}
+		long sum = 0;
+		for(int i=0 ; i<=n ; i++)
+		{
+			if(arr[i])
+			{
+				sum = sum+i;
+			}
+		}
+		System.out.println(sum);
 		s.close();
 	}
 
