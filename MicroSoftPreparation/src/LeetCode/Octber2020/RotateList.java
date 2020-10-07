40585d1e9e526641e61ad9fecd2a1ac11c4940ca
SARTHAK AGARWAL <agarwalsarthak21@gmail.com>
2020-10-07 22:43:11 +0530

    Leet Code October Challenge Problem-7

---
+++ MicroSoftPreparation/src/LeetCode/Octber2020/RotateList.java
@@ -0,0 +1,135 @@
+package LeetCode.Octber2020;
+
+import java.util.Scanner;
+
+// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3486/
+
+class ListNode
+{
+	int val;
+	ListNode next;
+	ListNode(){}
+	ListNode(int val)
+	{
+		this.val = val;
+	}
+	ListNode(int val, ListNode next)
+	{
+		this.val = val;
+		this.next = next;
+	}
+}
+
+public class RotateList 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start...");
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-->0)
+		{
+			ListNode head = null;
+			int n = s.nextInt();
+			while(n-->0)
+			{
+				head = InsertNodeAtEndInList(head, s.nextInt());
+			}
+			PrintList(head);
+			System.out.println();
+			head = rotateRight(head, s.nextInt());
+			PrintList(head);
+			System.out.println();
+		}
+		s.close();
+	}
+
+	private static ListNode rotateRight(ListNode head, int k) 
+	{
+		if(head == null)
+		{
+			return null;
+		}
+		if(head.next == null)
+		{
+			return head;
+		}
+		int len = LengthOfList(head);
+		int noOfRotations = k%len;
+		ListNode prev = null;
+		ListNode temp = head;
+		for(int i=0 ; i<len-noOfRotations ; i++)
+		{
+			prev = temp;
+			temp = temp.next;
+		}
+		temp = ReverseList(temp);
+		PrintList(head);
+		System.out.println();
+		while(temp != null)
+		{
+			prev.next = temp.next;
+			temp.next = head;
+			head = temp;
+			temp = prev.next;
+		}
+		return head;
+	}
+
+	private static ListNode ReverseList(ListNode root) 
+	{
+		if(root == null)
+		{
+			return null;
+		}
+		if(root.next == null)
+		{
+			return root;
+		}
+		ListNode temp = null, nextNode = null;
+		while(root != null)
+		{
+			nextNode = root.next;
+			root.next = temp;
+			temp = root;
+			root = nextNode;
+		}
+		return temp;
+	}
+
+	private static int LengthOfList(ListNode head)
+	{
+		int len = 0;
+		while(head!=null)
+		{
+			len++;
+			head = head.next;
+		}
+		return len;
+	}
+
+	private static void PrintList(ListNode head)
+	{
+		if(head != null)
+		{
+			System.out.print(head.val+" ");
+			PrintList(head.next);
+		}
+	}
+
+	private static ListNode InsertNodeAtEndInList(ListNode head, int k) 
+	{
+		if(head == null)
+		{
+			head = new ListNode(k);
+			return head;
+		}
+		ListNode temp = head;
+		while(temp.next != null)
+		{
+			temp = temp.next;
+		}
+		temp.next = new ListNode(k);
+		return head;
+	}
+}
